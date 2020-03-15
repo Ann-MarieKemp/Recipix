647d24bca6f5725e0deeac6db180a5d013fe167a
Ann-Marie Kemp <aedalenb@gmail.com>
2020-03-14 20:58:15 -0400

    updated styling on textLine element for return text

--- RecipixNative/Recipix/src/components/TextLine.js
+++ RecipixNative/Recipix/src/components/TextLine.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, TextInput, StyleSheet, PanResponder } from 'react-native';
-import { useState } from 'react';
 
-const TextLine = props => {
-  const [value, onChangeText] = useState(props.line);
-  return (
-    <TextInput
-      style={styles.textInput}
-      value={value}
-      onChangeText={text => {
-        onChangeText(text);
-      }}
-    />
-  );
-};
+class TextLine extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.line,
+    };
+    this.onChangeText = this.onChangeText.bind(this);
+  }
+  onChangeText(text) {
+    this.setState({ value: text });
+  }
+
+  render() {
+    return (
+      <TextInput
+        style={styles.textInput}
+        value={this.state.value}
+        onChangeText={text => {
+          this.onChangeText(text);
+        }}
+      />
+    );
+  }
+}
 export default TextLine;
 
 const styles = StyleSheet.create({
   textInput: {
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 6,
+    backgroundColor: '#ED6A5A',
+    borderRadius: 8,
+    color: 'white',
+    fontSize: 22,
+    margin: 2,
+    padding: 4,
   },
 });
