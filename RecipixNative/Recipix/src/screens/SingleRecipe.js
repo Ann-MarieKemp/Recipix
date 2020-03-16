cc49c2025382e0b4e8a820562190052dcfd906d4
Ann-Marie Kemp <aedalenb@gmail.com>
2020-03-15 20:28:35 -0400

    added styling to singleRecipe page

--- RecipixNative/Recipix/src/screens/SingleRecipe.js
+++ RecipixNative/Recipix/src/screens/SingleRecipe.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import firebase from '@react-native-firebase/app';
 const db = firebase.firestore();
@@ -17,8 +24,10 @@ class SingleRecipe extends Component {
   render() {
     return (
       <View style={styles.singleContainer}>
-        <Image source={this.state.recipe.recipePhoto} />
-        <Text style={styles.textStyle}>{this.state.recipe.instructions}</Text>
+        <Image style={styles.image} source={this.state.recipe.recipePhoto} />
+        <ScrollView style={styles.recipeCard}>
+          <Text style={styles.textStyle}>{this.state.recipe.instructions}</Text>
+        </ScrollView>
       </View>
     );
   }
@@ -28,16 +37,28 @@ export default SingleRecipe;
 const styles = StyleSheet.create({
   textStyle: {
     color: 'white',
-    fontSize: 22,
+    fontSize: 16,
     textAlign: 'center',
   },
   image: {
-    width: 50,
-    height: 50,
-    resizeMode: 'contain',
+    width: 100,
+    height: 100,
+    resizeMode: 'stretch',
+    marginTop: 10,
+    marginLeft: 5,
   },
   singleContainer: {
     width: width,
-    height: height,
+    height: 400,
+  },
+  recipeCard: {
+    width: width - 10,
+    alignSelf: 'center',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    marginBottom: 5,
+    borderRadius: 6,
+    backgroundColor: '#ED6A5A',
   },
 });
